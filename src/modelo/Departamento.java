871e8dda6417a0ca12dbd47d94b193be537d6e86
Your Name <you@example.com>
2024-09-28 13:41:40 -0300

    2º Parte
    Cada departamento tem um gerente responsável por supervisionar as atividades e um conjunto de funcionários que realizam as tarefas diárias.

--- src/modelo/Departamento.java
+++ src/modelo/Departamento.java
@@ -3,9 +3,18 @@ package modelo;
 /*Parte da questão que diz:
 A empresa possui uma estrutura organizada em departamentos, onde cada departamento é responsável por diferentes áreas de atuação, como Desenvolvimento de Software, Marketing e Recursos Humanos.
 */
+
+/*Parte 2 
+Cada departamento tem um gerente responsável por supervisionar as atividades e um conjunto de funcionários que realizam as tarefas diárias.
+*/
 public class Departamento {
     private String areaDeAtuacao;
-
+    // Eu criei como String pois ainda não li a questão toda
+    // e não sei o tipo/classe desse gerente.
+    private String gerente;
+    // Eu criei como String pois ainda não li a questão toda
+    // e não sei o tipo/classe desses funcionários.
+    private String listaFuncionarios [];
     public String getAreaDeAtuacao() {
         return areaDeAtuacao;
     }
