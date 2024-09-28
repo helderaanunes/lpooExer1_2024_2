8b82d616717c24e5136a55e845041454b23d8a4b
Your Name <you@example.com>
2024-09-28 15:20:39 -0300

    12º Parte
    Os funcionários podem ser promovidos a cargos de liderança de projetos, e essa experiência deve ser registrada no sistema para fins de avaliação de desempenho.

---
+++ src/modelo/LiderancaProjeto.java
@@ -0,0 +1,11 @@
+package modelo;
+
+/*
+ Os funcionários podem ser promovidos a cargos de liderança de
+projetos, e essa experiência deve ser registrada no sistema 
+para fins de avaliação de desempenho.
+ */
+public class LiderancaProjeto {
+    private Projeto projeto;
+    private Funcionario funcionario; 
+}
