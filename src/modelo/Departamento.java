0d0f02101fa365c1d5b0520a3dbd1849c77c2bce
Your Name <you@example.com>
2024-09-28 13:34:48 -0300

    1º Parte
    A empresa possui uma estrutura organizada em departamentos, onde cada departamento é responsável por diferentes áreas de atuação, como Desenvolvimento de Software, Marketing e Recursos Humanos.

---
+++ src/modelo/Departamento.java
@@ -0,0 +1,18 @@
+package modelo;
+
+/*Parte da questão que diz:
+A empresa possui uma estrutura organizada em departamentos, onde cada departamento é responsável por diferentes áreas de atuação, como Desenvolvimento de Software, Marketing e Recursos Humanos.
+*/
+public class Departamento {
+    private String areaDeAtuacao;
+
+    public String getAreaDeAtuacao() {
+        return areaDeAtuacao;
+    }
+
+    public void setAreaDeAtuacao(String areaDeAtuacao) {
+        this.areaDeAtuacao = areaDeAtuacao;
+    }
+    
+    
+}
